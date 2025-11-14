087706563addf9cb0e45adaa6551986b04928fab
Samuel Ward <samuelleeward9@gmail.com>
2025-11-14 22:55:20 +0100

    physics moving logo experiment

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,36 +1,64 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
+export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
+    player: Phaser.Physics.Arcade.Sprite;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
 
-    constructor ()
-    {
-        super('Game');
+    constructor() {
+        super("Game");
     }
 
-    create ()
-    {
+    create() {
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
 
-        this.background = this.add.image(512, 384, 'background');
+        this.background = this.add.image(512, 384, "background");
         this.background.setAlpha(0.5);
+        this.background.setDepth(0);
 
-        this.gameText = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        this.player = this.physics.add.sprite(512, 500, "logo");
+        this.player.setDepth(1);
+        this.player.setCollideWorldBounds(true);
 
-        EventBus.emit('current-scene-ready', this);
+        if (this.input.keyboard)
+            this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.gameText = this.add
+            .text(512, 384, "DRAGONSPLOIT", {
+                fontFamily: "Arial Black",
+                fontSize: 38,
+                color: "#ffffff",
+                stroke: "#000000",
+                strokeThickness: 8,
+                align: "center",
+            })
+            .setOrigin(0.5)
+            .setDepth(100);
+
+        EventBus.emit("current-scene-ready", this);
     }
 
-    changeScene ()
-    {
-        this.scene.start('GameOver');
+    update() {
+        this.player.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-300);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(300);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-300);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(300);
+        }
+    }
+
+    changeScene() {
+        this.scene.start("GameOver");
     }
 }
