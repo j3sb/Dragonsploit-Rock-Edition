556aea7c83a3c0df7cbd805cc076cf3f04c12b48
Jonas <jonas.bonnaudet@gmail.com>
2025-11-15 19:46:52 +0100

    credits again?

--- src/game/scenes/GameOver.ts
+++ src/game/scenes/GameOver.ts
@@ -6,6 +6,7 @@ export class GameOver extends Scene
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameOverText : Phaser.GameObjects.Text;
+    credits : Phaser.GameObjects.Text;
 
     constructor ()
     {
@@ -20,11 +21,18 @@ export class GameOver extends Scene
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
 
-        this.gameOverText = this.add.text(512, 384, 'Game Over\nGame made during a game jam by Jonas Hallin, Sabrina Bjurman, Anton Marketeg, Shrikant Jayaprakash, Wietse Jaarsma, Isac Lindh, Saduk, Manoj Axelsson, Victor Löfgren, Samuel Ward', {
+        this.gameOverText = this.add.text(512, 384, 'Game Over', {
             fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5).setDepth(100);
+
+        this.credits = this.add.text(512, 450, 'Game made during a game jam by Jonas Hallin, Sabrina Bjurman, Anton Marketeg,\n Shrikant Jayaprakash, Wietse Jaarsma, Isac Lindh, Saduk, Manoj Axelsson, Victor Löfgren, Samuel Ward', {
+            fontFamily: 'Arial Black', fontSize: 16, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5).setDepth(100);
+
         
         EventBus.emit('current-scene-ready', this);
     }
