8bbabf0fabfd0ae3ee9c8e407640dc4e167e07e6
Light-Wave <66904273+Light-Wave@users.noreply.github.com>
2025-11-17 08:44:01 +0100

    Removes buttons next to the game to make the game better fit in itch.io

--- src/App.tsx
+++ src/App.tsx
@@ -50,18 +50,6 @@ function App() {
     return (
         <div id="app">
             <PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
-            <div>
-                <div>
-                    <button className="button" onClick={changeScene}>
-                        Change Scene
-                    </button>
-                </div>
-                <div>
-                    <button className="button" onClick={addSprite}>
-                        Add New Sprite
-                    </button>
-                </div>
-            </div>
         </div>
     );
 }
